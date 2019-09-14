910afe4e1b8dfb556a6d4d5ad8830ad01cf4fb9c
Perrin Antonin <tom.per@orange.fr>
2019-09-14 15:47:07 +0200

    ajout d'ui

---
+++ StegaSoft/ReadPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace StegaSoft
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ReadPage : Page
+    {
+        public ReadPage()
+        {
+            this.InitializeComponent();
+        }
+        private async void SelectImage_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+
+            openPicker.FileTypeFilter.Add(".bmp");
+            //openPicker.FileTypeFilter.Add(".png");
+            string MessageDecoder;
+
+            StorageFile file = await openPicker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+
+                var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                var imageForDisplay = new BitmapImage();
+                imageForDisplay.SetSource(stream);
+                ImagePreview.Children.Add(new Image() { Source = imageForDisplay, Width = 300, Height = 300 });
+
+
+
+                Read imageDescript = new Read();
+                imageDescript.file = file;
+                MessageDecoder = await imageDescript.Operation();
+                Console.Write(MessageDecoder);
+                Result.Text = MessageDecoder;
+
+
+            }
+        }
+    }
+}
--- StegaSoft/WritePage.xaml.cs
+++ StegaSoft/WritePage.xaml.cs
@@ -10,34 +10,21 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace StegaSoft
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class WritePage : Page
     {
-
-        public MainPage()
+        public WritePage()
         {
-           
             this.InitializeComponent();
         }
-
-        private void HyperlinkButton_ClickRead(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(ReadPage));
-        }
-        private void HyperlinkButton_ClickWrite(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(WritePage), null);
-        }
-
-
     }
 }
-
