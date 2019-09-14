77069ecb30dce06d0fd58116711bab50ec34b6cd
Nathan Rinalduzzi <n.rinalduzzi@gmail.com>
2019-09-14 15:43:19 +0200

    Ajout d'un class pour gêrer le flux d'un fichier

---
+++ StegaSoft/FileDecimal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace StegaSoft
+{
+    public class FileDecimal
+    {
+        //base class, got a method who return the decimal stream of the file in param
+        protected int[] StreamDecimal;
+
+        protected async Task<int[]> GetDeicmalStream(StorageFile file, int indexBegin)
+        {
+            IBuffer buffer = await FileIO.ReadBufferAsync(file);
+            byte[] fileBytes = buffer.ToArray();
+            int[] sb = new int[fileBytes.Length];
+
+            for (int i = indexBegin; i < fileBytes.Length; i++)
+            {
+
+                sb[i - indexBegin] = fileBytes[i];
+
+            }
+
+            return sb;
+        }
+    }
+}
